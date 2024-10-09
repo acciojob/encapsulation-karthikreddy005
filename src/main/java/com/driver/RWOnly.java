2d8cd6baf3f19be9089629acc8d3c94e9b5713c8
THOKALA KARTHIK REDDY <117522702+karthikreddy005@users.noreply.github.com>
2024-10-09 22:45:28 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+
+    String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
     
 }
