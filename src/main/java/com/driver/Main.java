c9e24c0280adadabd4bee380b60c7b9255c9d8ba
THOKALA KARTHIK REDDY <117522702+karthikreddy005@users.noreply.github.com>
2024-10-09 22:54:50 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,9 @@
 package com.driver;
 
 public class Main {
-  RWOnly rw  = new RWOnly();
-  rw.setName("Karthik");
-System.out.println(rw.getName());
-  
+  public static void main(String[] args){
+    RWOnly rw  = new RWOnly();
+    rw.setName("Karthik");
+  System.out.println(rw.getName());
+  }
 }
